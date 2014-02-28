5d2a05feae208c98ee87d66affa71e6e0d7b78a8
Mitchell Barry <mitch.barry@gmail.com>
2014-02-27 18:32:47 -0600

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('localboardsUiReduxApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
