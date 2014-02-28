e3c0a47a3f1231add904fe46ad6d36c2f5bc57c3
Jay Hannah <jay.hannah@iinteractive.com>
2014-02-27 19:57:24 -0600

    detail view! woot!

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,12 +5,19 @@
 var localboardsControllers = angular.module('localboardsControllers', []);
 
 localboardsControllers.controller('BoardListCtrl', ['$scope', '$http',
-  function($scope, $http) {
+    function($scope, $http) {
+        $http.get('http://api.localboards.org/states/NE/boards').
+            success(function(data) {
+                $scope.boards = data.data;
+            }
+        );
+    }
+]);
 
-    $http.get('http://api.localboards.org/states/NE/boards').
-        success(function(data) {
-            $scope.boards = data.data;
-        }
-    );
+localboardsControllers.controller('BoardDetailCtrl', ['$scope', '$routeParams',
+    function($scope, $routeParams) {
+        $scope.boardId = $routeParams.boardId;
+    }
+]);
 
-  }]);
+
